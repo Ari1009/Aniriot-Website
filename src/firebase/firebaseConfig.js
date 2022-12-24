e631276a00a8f1911e7a2de7f91f08d21c5ab396
Arihant Pal <arihant0pal@gmail.com>
2022-12-25 00:11:39 +0530

    fix

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBlfU9yEcQbCT4JUAyO6qqIxQgkPlkVf7Q",
+    authDomain: "aniriot-ba920.firebaseapp.com",
+    databaseURl: "https://aniriot-ba920-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "aniriot-ba920",
+    storageBucket: "aniriot-ba920.appspot.com",
+    messagingSenderId: "71732724992",
+    appId: "1:71732724992:web:71d1637f5fba328b325fe5",
+    measurementId: "G-9Z49LD52H3"
+  };
